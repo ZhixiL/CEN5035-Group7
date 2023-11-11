607a603cc02d913b5b671cc5c919c991ec1bfd92
ZhixiL <zl19@my.fsu.edu>
2023-11-11 13:52:13 -0500

    Made GPTAPI work

--- GPTAPI.py
+++ GPTAPI.py
@@ -1,22 +1,64 @@
-import openai
-import os
-import pandas as pd
-openai.api_key = ''
+from openai import OpenAI, AsyncOpenAI
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0,
+initdi = {'prompt': "You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n. For each index i, names[i] and heights[i] denote the name and height of the ith person.Return names sorted in descending order by the people's heights.",
+                    "constraint": "n == names.length == heights.length"}
+
+#works fine
+def getOneResponse(dict):
+    client = OpenAI(
+        # defaults to os.environ.get("OPENAI_API_KEY")
+        api_key="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",
     )
 
-    return response.choices[0].message["content"]
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "Generate me a python3 funciton that does the follows: " + dict['prompt']
+            },
+            {
+                "role": "user",
+                "content": "Please re-generate providing the following constraints: " + dict['constraint']
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    print(chat_completion)
 
-prompt = '''Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. 
-            Return the answer in any order. 
-            A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.'''
+#Doesn't work
+def batchReq(dict):
+    client = OpenAI(
+        # defaults to os.environ.get("OPENAI_API_KEY")
+        api_key="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",
+    )
+    prompts = ["Generate me a python3 funciton that does the follows: " + dict['prompt'], "Please re-generate providing the following constraints: " +dict['constraint']]
+    
+    # batched example, with 10 story completions per request
+    response = client.completions.create(
+        model="gpt-3.5-turbo",
+        prompt=prompts,
+        max_tokens=100,
+    )
+    
+    # match completions to prompts by index
+    stories = [""] * len(prompts)
+    for choice in response.choices:
+        stories[choice.index] = prompts[choice.index] + choice.text
+    
+    # print stories
+    for story in stories:
+        print(story)
 
-response = get_completion(prompt)
+# be careful not to exceed 3 request per minute
+async def getBunchResponse(dict):
+    async_client = AsyncOpenAI()
+    stream = await async_client.chat.completions.create(
+        prompt="Say this is a test",
+        messages=[{"role": "user", "content": "Say this is a test"}],
+        stream=True,
+    )
+    async for part in stream:
+        print(part.choices[0].delta.content or "")
 
-print(response)
+if __name__ == "__main__":
+    getOneResponse(initdi)
