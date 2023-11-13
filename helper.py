d4342b1d19b9157ff1c0d7f4b917be5ff4ee85d1
Linwei Jiang <linwj1016@gmail.com>
2023-11-13 00:13:12 -0500

    added methods to obtain chatGPT similarity score and resulting csv file

--- helper.py
+++ helper.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import csv
+
 def writeToCSV(responseList, fileName, headers):
     df = pd.DataFrame(responseList, columns=headers)
     df.to_csv(fileName, index=False)  
 
 def readCSV(fileName):
-    return list(csv.reader(open(fileName)))
+    return list(csv.reader(open(fileName,encoding='UTF-8')))[1:]
